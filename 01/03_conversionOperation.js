c3812c310f9b1543e87c52c6877f05936177e2e0
naba079 <80685214+naba079@users.noreply.github.com>
2023-09-28 02:14:10 +0000

    conversion of datatype

---
+++ 01/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "23" = 23
+// "34aga" = NaN type, number
+// true = number, 1
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 = true, 0 = false
+// "" = false , "naba" = true
+
+let randomNumber = 45;
+let stringNumer = String(randomNumber)
+console.log(stringNumer)
+console.log(typeof stringNumer);
