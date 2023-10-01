4d835918b51353e019f741f3aeef5b799a67ec4d
naba079 <80685214+naba079@users.noreply.github.com>
2023-10-01 15:36:23 +0000

    function with obj and arrs

--- 03/01_function.js
+++ 03/01_function.js
@@ -22,4 +22,36 @@ function loginUserMessage(username = "Sam"){
 }
 
 const ans = loginUserMessage()
-console.log(ans);
+// console.log(ans);
+
+// ... = rest operator / spread operator
+function calculateCartPrice(...num1){
+    return num1
+}
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+// -->  val1 = 200, val2 = 400 only [600] will print then
+
+// console.log(calculateCartPrice(200,400,600))
+
+const user = {
+    username: "naba",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200,400,600]
+function returnSecondValue(getArray){
+    return getArray[2];
+}
+console.log(returnSecondValue(myNewArray))
